ad3f6ab7b67b30db8295cd3f6acd8f56900a4dbd
Kamil Szymkowski <szymkowski.k@gmail.com>
2023-02-23 18:34:12 +0100

    more specifications

---
+++ data_generator/sql_generator.py
@@ -0,0 +1,4 @@
+"""
+The purpose of this module is to generate SQL code for populating a relational database.
+Write functions that will generate insert stataments to populate DB with randomly generated values.
+"""
---
+++ skills_survey/skills_survey.py
@@ -0,0 +1,3 @@
+"""
+Create a User Interface (graphical or not) for employees to submit the skills they have and how many months of experience with each.
+"""
