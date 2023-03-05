1711fdae0cf80c8248dcbb032ca673f375fe5b94
Kamil Szymkowski <szymkowski.k@gmail.com>
2023-03-05 21:37:47 +0100

    wdw dir

---
+++ wdp/control_panel/control_panel.py
@@ -0,0 +1,5 @@
+# absolute
+from wdp.custom_loggers.general import logger
+# $env:PYTHONPATH = '.'
+
+# relative
---
+++ wdp/custom_loggers/profiler.py
@@ -0,0 +1,16 @@
+import sys
+
+print(sys.path)
+
+# C:\Projects\workforce-data-platform> &
+# "C:/Program Files/Python39/python.exe"
+# c:/Projects/workforce-data-platform/custom_loggers/profiler.py
+
+# ['c:\\Projects\\workforce-data-platform\\custom_loggers',
+#  'C:\\Program Files\\Python39\\python39.zip',
+#  'C:\\Program Files\\Python39\\DLLs',
+#  'C:\\Program Files\\Python39\\lib',
+#  'C:\\Program Files\\Python39',
+#  'C:\\Users\\szymk\\AppData\\Roaming\\Python\\Python39\\site-packages',
+#  'C:\\Program Files\\Python39\\lib\\site-packages']
+# PS C:\Projects\workforce-data-platform>
