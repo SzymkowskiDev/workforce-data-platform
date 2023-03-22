3c97c259ed20d77c3b841b37d423f8d43703d841
zdziebkowski <wojciech.zdziebkowski@gmail.com>
2023-03-22 22:47:14 +0100

    connector_wdp.py updated
    Able to use for all tables
    
    Added injection safe SQL queries

--- wdp/database/connector_wdp.py
+++ wdp/database/connector_wdp.py
@@ -1,27 +1,36 @@
 import sqlite3
 
-connection = sqlite3.connect("wdp_database.db")
-cursor = connection.cursor()
 
+class Database:
+    def __init__(self, database_name):
+        # create connection to database
+        self.connection = sqlite3.connect(database_name)
+        self.cursor = self.connection.cursor()
 
-def create_employee(employee):
-    with connection:
-        cursor.execute("INSERT INTO Employees VALUES (:employee_id,:first_name, :last_name, \
-        :avatar_url, :job_title, :joined_on, :email, :phone, :birthday, :country, :city, :project_id, \
-        :last_role, :role_preferred, :salary, :specification")
-        return print('Record created')
+    def __del__(self):
+        # close connection to database after function usage
+        self.connection.close()
 
+    def insert_sql(self, table, *values):
+        # insert values into table
+        self.cursor.execute(f"INSERT INTO {table} VALUES ({','.join(['?' for _ in values])})", values)
+        self.connection.commit()
+        print(f'Row inserted to {table} ')
 
-def read_employee(employee):
-    with connection:
-        cursor.execute("SELECT * FROM Employees WHERE employee_id=:employee_id")
-        return cursor.fetchall()
+    def select_sql(self, table, condition, value):
+        # select single condition equals value
+        self.cursor.execute(f"SELECT * FROM {table} WHERE {f'{condition}=?'}", value)
+        return print(self.cursor.fetchall())
 
+    def update_sql(self, table, id_column_name, id_num, *values):
+        # update multiple values by id
+        self.cursor.execute(f"UPDATE {table} SET ({','.join(['?' for _ in values])}) WHERE {id_column_name}={id_num}",
+                            values)
+        self.connection.commit()
+        print(f"ID {id_num} updated by {','.join(['?' for _ in values])} in {table}", values)
 
-def delete_employee(employee):
-    with connection:
-        cursor.execute("DELETE FROM Employees WHERE employee_id=:employee_id")
-        return print('Record deleted')
-
-
-connection.close()
+    def delete_sql(self, table, id_column_name, id_num):
+        # delete record by id
+        self.cursor.execute(f"DELETE FROM {table} WHERE {id_column_name}={id_num}")
+        self.connection.commit()
+        print(f'ID {id_num} deleted from {table}')
