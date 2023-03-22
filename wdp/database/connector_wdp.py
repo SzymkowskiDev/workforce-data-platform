d5ee07d9c651e2483b8b34ac6ba951c727e69cdd
zdziebkowski <wojciech.zdziebkowski@gmail.com>
2023-03-22 23:09:34 +0100

    connector_wdp.py db name as variable with default value

--- wdp/database/connector_wdp.py
+++ wdp/database/connector_wdp.py
@@ -2,9 +2,9 @@ import sqlite3
 
 
 class Database:
-    def __init__(self, database_name):
+    def __init__(self, database_name="wdp_database.db"):
         # create connection to database
-        self.connection = sqlite3.connect("wdp_database.db")
+        self.connection = sqlite3.connect(database_name)
         self.cursor = self.connection.cursor()
 
     def __del__(self):
