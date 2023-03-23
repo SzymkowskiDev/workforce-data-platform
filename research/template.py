9550dde891b4d00a39515a6f7e0e8a9e63195bdf
Pawel Paprzycki <pawel.paprzycki94@gmail.com>
2023-03-23 22:31:54 +0100

    Corrections after code review

---
+++ research/template.py
@@ -0,0 +1,17 @@
+data = {
+    "title": "",
+    "street": "",
+    "country_code": "",
+    "address_text": "",
+    "marker_icon": "",
+    "workplace_type": "",
+    "company_name": "",
+    "company_url": "",
+    "company_size": "",
+    "experience_level": "",
+    "id": "",
+    "employment_types": "",
+    "skills": "",
+    "remote": "",
+    "multilocation": ""
+}
